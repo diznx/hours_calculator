1b0f1ce1f6b6e6c19fdceed0e1d086faeb955f5d
diznx <85420656+diznx@users.noreply.github.com>
2022-10-17 21:16:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ totalMinutes = int(input("and minutes? "))
 remainingHours = targetHours - totalHours
 remainingMinutes = 60 - totalMinutes
 
-hourIn = int(input("Hour clocked in? "))
-minuteIn = int(input("The minute? "))
+hourIn = int(input("What hour did you clock in? "))
+minuteIn = int(input("and the minute? "))
 
 hourOut = hourIn + remainingHours
 minuteOut = minuteIn + remainingMinutes
+
 
 if minuteOut > 59:
     hourOut = hourOut + 1
@@ -20,5 +21,4 @@ if minuteOut > 59:
 minuteOut = minuteOut % 60
 
 print("The time left you have to work is", remainingHours, "hours and", remainingMinutes, "minutes.")
-
 print("You need to clock out at ", hourOut, ":", minuteOut, sep="")
