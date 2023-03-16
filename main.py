7bbdf2f8479fa7ff10f53e8d153756ee3c79f6b4
diznx <85420656+diznx@users.noreply.github.com>
2023-03-16 11:21:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-print("Hours calculator")
-
-targetHours = 84
-
-totalHours = int(input("How many total hours did you have after last day of work? "))
-totalMinutes = int(input("and minutes? "))
-
-remainingHours = targetHours - totalHours
-remainingMinutes = 60 - totalMinutes
-
-hourIn = int(input("What hour did you clock in? "))
-minuteIn = int(input("and the minute? "))
-
-hourOut = hourIn + remainingHours
-minuteOut = minuteIn + remainingMinutes
-
-
-if minuteOut > 59:
-    hourOut = hourOut + 1
-
-minuteOut = minuteOut % 60
-
-print("The time left you have to work is", remainingHours, "hours and", remainingMinutes, "minutes.")
-print("You need to clock out at ", hourOut, ":", minuteOut, sep="")
+target_minutes = 5040
+total_hours = int(input("Total hours: ")) * 60
+total_minutes = int(input("and minutes: "))
+total_time = total_hours + total_minutes
+hour_in = int(input("Hour clocked in: "))
+minute_in = int(input("Minute in: "))
+hour_left = (target_minutes - total_time) // 60
+minute_left = (target_minutes - total_time) % 60
+print(f"You have {hour_left} hours and {minute_left} minutes left.")
+hour_out = hour_in + hour_left
+minute_out = (minute_in + minute_left) % 60
+if minute_out >= 0.60:
+    hour_out = hour_out + 1
+print(f"Clock out at {hour_out}:{minute_out}.")
